854826c4711e6d67e3fc565078c95383940db8f4
yoshi <yoshiaki11205656@gmail.com>
2020-12-07 21:28:30 +0900

    props

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Basic1 = () => {
+const Basic1 = ( props ) => {
     return (
         <React.Fragment>
-            <h1>Hello World!</h1>
+            <h1>Hello {props.name}!</h1>
             <h1>Hello World!</h1>
         </React.Fragment>
     )
