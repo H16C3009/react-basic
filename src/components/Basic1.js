ad714764bf54d82cfaec5ee7857f7a626d649530
yoshi <yoshiaki11205656@gmail.com>
2020-12-07 21:24:51 +0900

    functional component

---
+++ src/components/Basic1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic1 = () => {
+    return (
+        <React.Fragment>
+            <h1>Hello World!</h1>
+            <h1>Hello World!</h1>
+        </React.Fragment>
+    )
+}
+
+export default Basic1
