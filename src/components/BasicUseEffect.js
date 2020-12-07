cc67a82b398745970e7da37433df3633ce58a07a
yoshi <yoshiaki11205656@gmail.com>
2020-12-07 23:34:44 +0900

    useEffect conditinal render

--- src/components/BasicUseEffect.js
+++ src/components/BasicUseEffect.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect} from 'react'
 
 const BasicUseEffect = () => {
     const [ count, setCount ] = useState(0)
+    const [ item, setItem ] = useState('')
 
     useEffect(() => {
         console.log('useEffect invoked')
-    }, [])
+    }, [count])
 
     return (
         <>
             <button onClick={ () => setCount( preCount => preCount+1)} >click {count}</button>
+            <input type='text' value={item} onChange={ e => setItem( e.target.value )} />
         </>
     )
 }
