ef30cf7df6d5743dd65c70f269ebdddf8cadf2f6
yoshi <yoshiaki11205656@gmail.com>
2020-12-07 21:39:44 +0900

    useState basic1

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Basic1 = ( props ) => {
     const clickHandler = () => {
         console.log('clicked')
     }
 
+    const [ count, setCount ] = useState(0)
+
     return (
         <React.Fragment>
-            <button onClick={clickHandler}>click</button>
+            <button onClick={ () => setCount(count+1) }>count {count}</button>
             <h1>Hello {props.name}!</h1>
             <h1>Hello World!</h1>
         </React.Fragment>
