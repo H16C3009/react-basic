f2d823de8dbd36bb673658ba930936366248729a
yoshi <yoshiaki11205656@gmail.com>
2020-12-07 21:32:15 +0900

    basic event handler

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Basic1 = ( props ) => {
+    const clickHandler = () => {
+        console.log('clicked')
+    }
+
     return (
         <React.Fragment>
+            <button onClick={clickHandler}>click</button>
             <h1>Hello {props.name}!</h1>
             <h1>Hello World!</h1>
         </React.Fragment>
